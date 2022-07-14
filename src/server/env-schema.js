111ace241381fcd21e131efea17a64ad86706807
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-15 01:06:04 +0200

    env-schema change

--- src/server/env-schema.js
+++ src/server/env-schema.js
@@ -4,7 +4,6 @@ const envSchema = z.object({
   NEXTAUTH_SECRET: z.string(),
   NEXTAUTH_URL: z.string().url(),
   DATABASE_URL: z.string().url(),
-  DATABASE_NAME: z.string(),
 });
 
 module.exports.envSchema = envSchema;
